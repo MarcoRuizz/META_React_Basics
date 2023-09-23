d3c74eedadbd03b75094b948e163663a2d77eed1
MarcoRuizz <marco27america@gmail.com>
2023-09-23 11:03:42 -0600

    passing drops excercise

---
+++ module1/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firtName="Marco"/>
+            <Heading firtName="Juan"/>
+        </div>
+    );
+};
+
+export default App;
--- projects/primer-react-app/src/components/Heading.tsx
+++ projects/primer-react-app/src/components/Heading.tsx
@@ -1,8 +1,10 @@
 // to import this component in App.tsx: import Heading from './components/Heading';
 
+
+// heading is action as child component from App.tsx because is receiving a prop
 function Heading(props : {label: string}) {
         return (
-            <h1>This is a {props.label}</h1>
+            <h1>This is a {props.label} label</h1>
         )
     }
     
