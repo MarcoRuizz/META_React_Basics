7e3f1a4ddbf55ff5e88ed63528cd1bcf7cc126d2
MarcoRuizz <marco27america@gmail.com>
2023-09-23 20:30:36 -0600

    props expressions

---
+++ module1/PropsExpresssions.tsx
@@ -0,0 +1,27 @@
+// you can pass almost any kind of expression as a prop.
+const bool = false;
+const str1 = "just";
+
+function Example(props) {
+    return (
+        <div>
+            <h2>
+                The value of the toggleBoolean prop is:{props.toggleBoolean.toString()}
+            </h2>
+            <p>The value of the math prop is: <em>{props.math}</em></p>
+            <p>The value of the str prop is: <em>{props.str}</em></p>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <div className="App">
+            <Example
+                toggleBoolean={!bool}
+                math={(10 + 20) / 3}
+                str={str1 + ' another ' + 'string'}
+            />
+        </div>
+    );
+};
