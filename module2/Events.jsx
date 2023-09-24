eaa36ad591aa32b9079c429387d90832264b6dfe
MarcoRuizz <marco27america@gmail.com>
2023-09-24 07:39:10 -0600

    events

---
+++ module2/Events.jsx
@@ -0,0 +1,11 @@
+//  proccess when the user manipulates the page, such as click or keyboard events
+
+ondrag
+ondrop
+onfocus
+onclick
+onchange
+
+// Event Errors
+
+(5).toUpperCase() -> Uncaught TypeError: 5.toUpperCase is not a function
